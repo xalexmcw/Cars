ea9f3790f41438d294a92e798332ecf1a5b44d8b
xalexmcw <xalexmcw@gmail.com>
2017-06-02 15:43:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,28 +4,26 @@ var wrapper = document.getElementById('content');
 fetch(url2)
     .then((resp) => resp.json())
     .then(function (data) {
-        console.log('CAR data', data);
-        for (var make in data) {
-            console.log('prva petlja', data);
-            for (var i = 0; i < data[make].length; i++) {
-                console.log('druga petlja', data[make][i]);
-                wrapper.innerHTML += '<div class="flip-container">\
+        console.log(data);
+        var cars = data.data;
+        for (var i = 0; i < cars.length; i++) {
+            wrapper.innerHTML += '<div class="flip-container">\
                                         <div class="flipper">\
                                             <div class="front">\
-                                                <h3>'+ data[make][i].name + '</h3>\
-                                                <img src="'+ data[make][i].image + '">\
+                                                <h3>'+ cars[i].name + '</h3>\
+                                                <img src="'+ cars[i].image + '">\
                                             </div >\
-                                        <div class="back" >\
+                                        <div class="back">\
                                             <section>\
-                                            <div class="bg" style="background-image: url('+data[make][i].image+');\
+                                            <div class="bg" style="background-image: url('+ cars[i].image + ');\
                                                                  background-size:cover;\
                                                                  background-color:#dadada;\
                                                                  background-repeat: no-repeat;\
                                                                  background-position: center;\
                                                                  \
                                                                  "></div>\
-                                            <h4> Description:</h4> <p> '+ data[make][i].description + '</p>\
-                                            <p> Speed: '+ data[make][i].speed + '</p>\
+                                            <h4> Description:</h4> <p> '+ cars[i].description + '</p>\
+                                            <p> Speed: '+ cars[i].speed + '</p>\
                                                 <div class="checkboxCon">\
                                                     <span class="circle"></span>\
                                                     <span class="secondLine"></span>\
@@ -34,13 +32,11 @@ fetch(url2)
                                         </div>\
                                     </div>\
                                 </div>'
-            }
         }
-
-        $(function () {
+        
+                $(function () {
             $('.checkboxCon').click(function () {
                 $(this).toggleClass('active');
             })
         });
-
     });
